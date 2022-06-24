f15fc581d4d607409a871329c9f520443651222d
Samantha Denis <sami.m.denis@gmail.com>
2022-06-24 15:19:32 -0400

    Auth, NewOrder, OrderHistory, SignUp, NavBar setup

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -7,7 +7,7 @@ import { Routes, Route } from "react-router-dom"
 import NavBar from "../../components/NavBar/NavBar"
 
 export default function App() {
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState(null)
   return (
     <main className="App">
       {user ?
